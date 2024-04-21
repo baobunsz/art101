49bf079afa9d9dfd088dd9a0b09a780cc124efd0
baobunsz <bhdang25@gmail.com>
2024-04-20 18:16:40 -0700

    lab05-update

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * Author: Bao Hy Dang ,bdang9@ucsc.edu>
+ * Created: 20 April
+ * License: Public Domain
+ */
+ 
+// Define variables
+make = "Tesla";
+model = "X";
+color = "white";
+year = 2015;
 
-// Constants
+// calculate
+age = 2024 - year;
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
