4bcc1711e60f8a3a4baf5fba627bf3a05fa0abb1
baobunsz <bhdang25@gmail.com>
2024-06-06 21:12:07 -0700

    lab16(notwork)

---
+++ lab16/js/lab.js
@@ -0,0 +1,45 @@
+/*
+   lab.js - AJAX and JQUERY
+
+   Requirements: jQuery must be loaded for this script to work.
+   Author: Bao Hy Dang
+   Date: June 6th, 2024
+*/
+
+//declare the URL
+const URL = "https://xkcd.com/info.0.json";
+
+//set up ajax object
+const comicObj = {
+   url: URL,
+   // data: {},
+   type: "GET",
+   dataType : "json",
+   success: ajaxSuccess,
+   error: ajaxError
+};
+
+// create ajax success callback (named)
+function ajaxSuccess(data) {
+  // console.log("Data:", data);
+  // parse json
+  const comic = data.img;
+  const title = data.title;
+  const alt = data.alt;
+
+
+  // put comic in output div
+  $("#output").html("<div>" + comic + "</div>");
+  $("#output").prepend("<div>" + title + "</div>");
+  $("#output").append("<div>" + alt + "</div>");
+}
+
+
+// create ajax error callback
+function ajaxError(request,error){
+	  console.log("Oops:", request, error);
+}
+
+
+
+
